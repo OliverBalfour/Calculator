08a46e42ebfdc47ba19e3aefb933d9406a4f4c89
Oliver Balfour <oliver.leo.balfour@gmail.com>
2020-07-29 12:04:46 +1000

    Latex \frac

--- Arithmetic.hs
+++ Arithmetic.hs
@@ -6,8 +6,15 @@ import Control.Monad (unless)
 import Data.List (filter)
 
 expr :: Parser Double
-expr = factor `chainr1` powop `chainl1` mulop `chainl1` addop
-factor = number <|> constant <|> brackets
+-- todo: data structure storing operations along with their fixity and precedence
+-- so we can dynamically add infixr7, prefix9 etc functions/operators
+-- todo: functions
+-- todo: latex expressions (eg \div is a prefix function)
+-- todo: multiplication without sign (tricky)
+expr = subexpr `chainr1` powop `chainl1` mulop `chainl1` addop
+
+-- todo: unary - operator, 10e-2 scientific notation
+subexpr = number <|> constant <|> function <|> brackets
 
 constant = foldr1 (<|>) $ map
   (\(cs, val) -> symb cs *> return val)
@@ -18,6 +25,12 @@ brackets = foldr1 (<|>) $ map
   [("(",")"), ("[","]"), ("{","}"),
   ("\\left(","\\right)"), ("\\left[","\\right]"), ("\\left{","\\right}")]
 
+function = do
+  symb "\\frac"
+  a <- subexpr
+  b <- subexpr
+  return (a / b)
+
 powop :: Parser (Double -> Double -> Double)
 powop = (symb "**" <|> symb "^") *> return (**)
 
@@ -27,14 +40,19 @@ addop = (symb "+" *> return (+)) <|> (symb "-" *> return (-))
 mulop :: Parser (Double -> Double -> Double)
 mulop = ((symb "*" <|> symb "\\times") *> return (*)) <|> (symb "/" *> return (/))
 
-prettyExpr :: String -> Double
-prettyExpr = fst . (!!0) . apply expr
+prettyExpr :: String -> String
+prettyExpr cs = let x = apply expr cs in
+  if (length x == 0)
+    then "No results"
+    else if (length (snd (x !! 0)) /= 0)
+      then "Invalid character encountered: " ++ snd (x !! 0)
+      else show $ fst (x !! 0)
 
 test :: IO [()]
 test = sequence $ map printFailed (filter (not . testPasses) tests)
   where
     printFailed = \(cs, out) -> putStrLn $ "error: " ++ cs ++ " == " ++ show out
-    testPasses (cs, out) = (prettyExpr cs) `dblEq` out
+    testPasses (cs, out) = (fst . (!!0) . apply expr $ cs) `dblEq` out
     a `dblEq` b = abs (a - b) < 0.0001
     tests = [
       ("1+2", 3.0),
@@ -52,4 +70,4 @@ main :: IO ()
 main = do
   test
   putStrLn "Calculator"
-  interact $ unlines . map (show . prettyExpr) . lines
+  interact $ unlines . map prettyExpr . lines
